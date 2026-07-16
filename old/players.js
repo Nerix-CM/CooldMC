650cc9b7b839861416bacfd4b606531a939aba5c
_Nerix <andrew.sobakin2011@gmail.com>
2026-07-16 22:13:34 +0300

    Create players.js

---
+++ old/players.js
@@ -0,0 +1,29 @@
+// players.js - файл с данными игроков сервера CooldMC
+
+/*
+gl_admin ГЛ.Администратор (&c)
+admin Администратор (&c)
+builder Билдер (&e)
+gl_moderator ГЛ.Модератор (&9)
+st_moderator СТ.Модератор (&9)
+moderator Модератор (&9)
+gl_helper ГЛ.Хелпер (&6)
+st_helper СТ.Хелпер (&6)
+helper Хелпер (&6)
+beta Бета-тестер (&e)
+*/
+
+const playersData = [
+    { name: "_Nerix", role: "admin", joinDate: "2025-09-11" },
+    
+    { name: "Tarkos0", role: "gl_moderator", joinDate: "2025-09-12" },
+    
+    // Обычные игроки (без роли)
+    { name: "Okosan2", role: null, joinDate: "2025-12-20" },
+    { name: "faks52", role: null, joinDate: "2025-11-12" },
+    { name: "Skortek", role: null, joinDate: "2025-09-13" },
+    { name: "Ded7s", role: null, joinDate: "2025-12-26" },
+    { name: "_Zora_", role: "moderator", joinDate: "2025-11-25"},
+    { name: "_Enflis_", role: null, joinDate: "2026-04-25"}
+    //{ name: "Devo4ka_Me4t", role: "builder", joinDate: "2026-06-12"}
+];
